1c6c735d18881934aa6b9f229cbeaed432f45b25
Hasil Paudyal <hasilpaudyal@gmail.com>
2019-12-06 22:13:53 +0545

    vue native initialized with homepage

---
+++ components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="card">
+      <text class="title">{{title}}</text>
+      <text class="content">{{content}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ['title', 'content']
+};
+</script>
+
+<style>
+.card {
+    margin: 20px;
+    padding: 20px 10px;
+    background-color: #fff;
+    borderRadius: 40px;
+}
+.title {
+    color: #e6a410;
+    font-size: 30px;
+    font-weight: 500;
+}
+.content {
+    color: #6e161c;
+    font-size: 20px;
+    text-align: justify;
+    margin-top: 20px;
+}
+</style>
